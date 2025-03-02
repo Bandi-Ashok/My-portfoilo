ed59bbbda95a4205c6f2682984f6a71fc0144a66
Bandi-Ashok <bandibandiashokkumar@gmail.com>
2025-03-02 12:11:04 +0530

    intial commit

---
+++ assets/css/assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    Swal.fire({
+        title: "Welcome!",
+        text: "Thank you for visiting my portfolio.",
+        icon: "info",
+        confirmButtonText: "Explore"
+    });
+});
--- script.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Welcome to Bandi Ashok Kumar's Portfolio!");
-});
